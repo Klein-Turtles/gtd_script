496ae4315d9411c6d04e5101bc60a71eeb7b5953
Tegar Jatu Ramadhani <tegarjr634@gmail.com>
2026-04-23 13:40:38 +0700

    Update gtd.lua

--- gtd.lua
+++ gtd.lua
@@ -55,7 +55,7 @@ Tab:CreateButton({
 Tab:CreateButton({
     Name = "⚡ Buy Teslaflora",
     Callback = function() 
-        snipeProduct("dp_unit_choco_tesla") 
+        snipeProduct("dp_unit_tesla") 
     end,
 })
 
